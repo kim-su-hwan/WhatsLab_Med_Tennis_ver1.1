811d4cd5c5f1399bb398891c9986b2d10b95bc26
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2023-02-03 19:16:17 +0900

    Setting oculus & ball
    asdf

--- Assets/2_Scripts/BallMove.cs
+++ Assets/2_Scripts/BallMove.cs
@@ -7,28 +7,51 @@ public class BallMove : MonoBehaviour
 {
 
     private Rigidbody rb;
-    private Vector3 dir = new Vector3(0,0,0);
+    private Vector3 dir = new Vector3(0, 0, 0);
     [SerializeField]
     private float speed = 20.0f;
     [SerializeField]
-    private float gravity = 10.6f;
+    private float gravity = 5.0f;
+    [SerializeField]
+    private float bounce = 30.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(dir.x * speed, dir.y*speed/3, dir.z * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(dir.x*speed, 0, dir.z*speed);
-        rb.AddForce(Vector3.down * gravity);
+        //rb.velocity = new Vector3(dir.x * speed, 0, dir.z * speed);
+        //rb.AddForce(Vector3.down * gravity);
     }
 
     public void SetDir(Transform target)
     {
         dir = target.transform.position - gameObject.transform.position;
         dir = dir.normalized;
+
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Racket")
+        {
+
+        }
+        if(other.tag =="court")
+        {
+            rb.AddForce(Vector3.up * bounce);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Racket")
+        {
+
+        }
+    }
 }
